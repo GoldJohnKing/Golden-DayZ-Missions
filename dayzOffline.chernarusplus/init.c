b2f46ced39509696d648a1c883ff1181e03d56a1
Gold John King <GoldJohnKing@Live.cn>
2021-11-01 00:15:34 +0800

    Tweak: Better player spawn equipment

--- dayzOffline.chernarusplus/init.c
+++ dayzOffline.chernarusplus/init.c
@@ -72,36 +72,96 @@ class CustomMission: MissionServer
 		ItemBase itemBs;
 		float rand;
 
-		itemClothing = player.FindAttachmentBySlotName( "Body" );
+		// Edited: Remove all player items
+		player.RemoveAllItems();
+
+		// Edited: Get player's reference
+		itemClothing = player;
+
+		// itemClothing = player.FindAttachmentBySlotName( "Body" ); // Edited: No need to place items only in clothes
 		if ( itemClothing )
 		{
-			SetRandomHealth( itemClothing );
+			// Edited: Add clothes to player
+			string clothesArray[] = { "Blouse_Blue","Blouse_Green","Blouse_Violet","Blouse_White","PrisonUniformJacket","Shirt_BlueCheck","Shirt_BlueCheckBright","Shirt_GreenCheck","Shirt_PlaneBlack","Shirt_RedCheck","Shirt_WhiteCheck" };
+			int rndIndex = Math.RandomInt( 0, 11 );
+			itemEnt = itemClothing.GetInventory().CreateInInventory( clothesArray[rndIndex] );
+			SetRandomHealth( itemEnt );
+
+			// Edited: Add pants to player
+			string pantsArray[] = { "CanvasPants_Beige","CanvasPants_Blue","CanvasPants_Grey","CanvasPants_Red","CanvasPants_Violet","Breeches_Beetcheck","Breeches_Beige","Breeches_Black","Breeches_Blackcheck","Breeches_Blue","Breeches_Browncheck","Breeches_Green","Breeches_Pink","Breeches_Red","Breeches_White" };
+			rndIndex = Math.RandomInt( 0, 15 );
+			itemEnt = itemClothing.GetInventory().CreateInInventory( pantsArray[rndIndex] );
+			SetRandomHealth( itemEnt );
+
+			// Edited: Add backpack to player
+			string backpackArray[] = { "ChildBag_Blue","ChildBag_Green","ChildBag_Red","CourierBag","FurCourierBag" };
+			rndIndex = Math.RandomInt( 0, 5 );
+			itemEnt = itemClothing.GetInventory().CreateInInventory( backpackArray[rndIndex] );
+			SetRandomHealth( itemEnt );
+
+			// Edited: Add shoes to player
+			string shoesArray[] = { "HikingBootsLow_Beige","HikingBootsLow_Black","HikingBootsLow_Blue","HikingBootsLow_Grey" };
+			rndIndex = Math.RandomInt( 0, 4 );
+			itemEnt = itemClothing.GetInventory().CreateInInventory( shoesArray[rndIndex] );
+			SetRandomHealth( itemEnt );
+
+			// SetRandomHealth( itemClothing ); // Edited: We've already done it before
 			
 			itemEnt = itemClothing.GetInventory().CreateInInventory( "BandageDressing" );
 			if ( Class.CastTo( itemBs, itemEnt ) )
-				itemBs.SetQuantity( 2 );
+				itemBs.SetQuantity( 4 ); // Edited: Increase bandage quantity, default = 2
 
 			string chemlightArray[] = { "Chemlight_White", "Chemlight_Yellow", "Chemlight_Green", "Chemlight_Red" };
-			int rndIndex = Math.RandomInt( 0, 4 );
+			rndIndex = Math.RandomInt( 0, 4 ); // Edited: Avoid duplicated defination, default = int rndIndex
 			itemEnt = itemClothing.GetInventory().CreateInInventory( chemlightArray[rndIndex] );
 			SetRandomHealth( itemEnt );
 
-			rand = Math.RandomFloatInclusive( 0.0, 1.0 );
-			if ( rand < 0.35 )
-				itemEnt = player.GetInventory().CreateInInventory( "Apple" );
-			else if ( rand > 0.65 )
-				itemEnt = player.GetInventory().CreateInInventory( "Pear" );
-			else
-				itemEnt = player.GetInventory().CreateInInventory( "Plum" );
+			// rand = Math.RandomFloatInclusive( 0.0, 1.0 ); // Edited: Disable default random food spawn
+			// if ( rand < 0.35 )
+			// 	itemEnt = player.GetInventory().CreateInInventory( "Apple" );
+			// else if ( rand > 0.65 )
+			// 	itemEnt = player.GetInventory().CreateInInventory( "Pear" );
+			// else
+			// 	itemEnt = player.GetInventory().CreateInInventory( "Plum" );
 
+			// SetRandomHealth( itemEnt );
+
+			// Edited: Add basic food to player
+			string basicFoodsArray[] = { "Apple","Pear","Plum","Tomato","GreenBellPepper","Potato","SlicedPumpkin" };
+			rndIndex = Math.RandomInt( 0, 7 );
+			itemEnt = itemClothing.GetInventory().CreateInInventory( basicFoodsArray[rndIndex] );
+			SetRandomHealth( itemEnt );
+
+			// Edited: Add solid food to player
+			string solidFoodsArray[] = { "Chips","Crackers","Rice","SaltySticks" };
+			rndIndex = Math.RandomInt( 0, 4 );
+			itemEnt = itemClothing.GetInventory().CreateInInventory( solidFoodsArray[rndIndex] );
+			SetRandomHealth( itemEnt );
+
+			// Edited: Add drink to player
+			string drinkArray[] = { "SodaCan_Cola","SodaCan_Kvass","SodaCan_Pipsi","SodaCan_Spite" };
+			rndIndex = Math.RandomInt( 0, 4 );
+			itemEnt = itemClothing.GetInventory().CreateInInventory( drinkArray[rndIndex] );
+			SetRandomHealth( itemEnt );
+
+			// Edited: Add knife to player
+			string knivesArray[] = { "SteakKnife","HuntingKnife","KitchenKnife" };
+			rndIndex = Math.RandomInt( 0, 3 );
+			itemEnt = player.GetInventory().CreateInInventory( knivesArray[rndIndex] );
+			SetRandomHealth( itemEnt );
+
+			// Edited: Add tent to player
+			string tentsArray[] = { "MediumTent","MediumTent_Green","MediumTent_Orange" };
+			rndIndex = Math.RandomInt( 0, 3 );
+			itemEnt = player.GetInventory().CreateInInventory( tentsArray[rndIndex] );
 			SetRandomHealth( itemEnt );
 		}
 		
-		itemClothing = player.FindAttachmentBySlotName( "Legs" );
-		if ( itemClothing )
-			SetRandomHealth( itemClothing );
+		// itemClothing = player.FindAttachmentBySlotName( "Legs" ); // Edited: We've already done it before
+		// if ( itemClothing )
+		// 	SetRandomHealth( itemClothing );
 		
-		itemClothing = player.FindAttachmentBySlotName( "Feet" );
+		// itemClothing = player.FindAttachmentBySlotName( "Feet" );
 	}
 };
 
